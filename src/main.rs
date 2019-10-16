8d56ef4b9e4f2037e42a6ca6087a9f3865380cf7
Logarithmus <logarithmus.dev@gmail.com>
2019-10-17 02:07:37 +0300

    It works!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+use std::collections::HashMap;
 use curl::easy::Form;
 use std::io::Write;
 use curl::easy::Easy;
@@ -21,13 +22,25 @@ fn send_request(easy: &mut Easy) -> Result<String, Box<dyn Error>> {
     Ok(response)
 }
 
-fn auth_cookie(auth_url: &str, cookie: &str) -> Result<String, Box<dyn Error>> {
+fn get_with_cookie(url: &str, cookie: &str) -> Result<String, Box<dyn Error>> {
 	let mut easy = Easy::new();
-	easy.url(auth_url)?;
+	easy.url(url)?;
 	easy.useragent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36")?;
 	easy.follow_location(true)?;
 	//easy.verbose(true)?;
 	easy.cookie(cookie)?;
+	let response = send_request(&mut easy)?;
+	Ok(response)
+}
+
+fn post_with_cookie(url: &str, cookie: &str, form: Form) -> Result<String, Box<dyn Error>> {
+	let mut easy = Easy::new();
+	easy.url(url)?;
+	easy.useragent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36")?;
+	easy.follow_location(true)?;
+	//easy.verbose(true)?;
+	easy.cookie(cookie)?;
+	easy.httppost(form)?;
 	let response = send_request(&mut easy)?;
 	Ok(response)
 }
@@ -38,36 +51,47 @@ fn get_question_form(html: &str) -> Result<Form, curl::FormError> {
 		static ref ID: Regex = Regex::new(r#"(?s)name="id".*?value="(.*?)""#).unwrap();
 		static ref COURSEID: Regex = Regex::new(r#"(?s)name="courseid".*?value="(.*?)""#).unwrap();
 		static ref SESSKEY: Regex = Regex::new(r#"(?s)name="sesskey".*?value="(.*?)""#).unwrap();
-		static ref USECURRENTCAT: Regex = Regex::new(r#"(?s)name="usecurrentcat".*?value="(.*?)""#).unwrap();
 		static ref NAME: Regex = Regex::new(r#"(?s)name="name".*?value="(.*?)""#).unwrap();
-		static ref QUESTIONTEXT: Regex = Regex::new(r#"(?s)name="questiontext\[text\]".*?>(.*?)<"#).unwrap();
+		static ref QUESTION_TEXT: Regex = Regex::new(r#"(?s)name="questiontext\[text\]".*?>(.*?)<"#).unwrap();
+		static ref QUESTION_FORMAT: Regex = Regex::new(r#"(?s)name="questiontext\[format\]".*?value="(.*?)""#).unwrap();
 		static ref DEFAULTMARK: Regex = Regex::new(r#"(?s)name="defaultmark".*?value="(.*?)""#).unwrap();
 		static ref SINGLE: Regex = Regex::new(r#"(?s)name="single".*?value="(.*?)""#).unwrap();
-		static ref SHUFFLEANSWERS: Regex = Regex::new(r#"(?s)name="shuffleanswers".*?value="(.*?)""#).unwrap();
 		static ref ANSWERNUMBERING: Regex = Regex::new(r#"(?s)name="answernumbering".*?value="(.*?)""#).unwrap();
-		static ref ANSWER_TEXT: Regex = Regex::new(r#"(?s)name="answer\[.*?\]\[text\]".*?>(.*?)<"#).unwrap();
-		static ref FRACTION: Regex = Regex::new(r#"(?s)name="fraction\[.*?\]".*?value="(.*?)".*?selected"#).unwrap();
+		static ref ANSWER_TEXT: Regex = Regex::new(r#"(?s)name="answer\[[0-9]+\]\[text\]".*?>(.*?)<"#).unwrap();
+		static ref ANSWER_FORMAT: Regex = Regex::new(r#"(?s)name="answer\[[0-9]+\]\[format\]".*?value="(.*?)""#).unwrap();
+		static ref FRACTION: Regex = Regex::new(r#"(?s)name="fraction\[[0-9]+\]".*?value="([0-9|\.]+)"\s+selected"#).unwrap();
 		static ref PENALTY: Regex = Regex::new(r#"(?s)name="penalty".*?value="(.*?)".*?selected"#).unwrap();
+
 	}
 	let mut form = Form::new();
-	form.part("noanswers").contents(NOANSWERS.captures(html).unwrap()[1].as_bytes()).add()?;
+	
 	form.part("id").contents(ID.captures(html).unwrap()[1].as_bytes()).add()?;
 	form.part("courseid").contents(COURSEID.captures(html).unwrap()[1].as_bytes()).add()?;
 	form.part("sesskey").contents(SESSKEY.captures(html).unwrap()[1].as_bytes()).add()?;
-	form.part("usecurrentcat").contents(USECURRENTCAT.captures(html).unwrap()[1].as_bytes()).add()?;
 	form.part("name").contents(NAME.captures(html).unwrap()[1].as_bytes()).add()?;
-	form.part("questiontext").contents(QUESTIONTEXT.captures(html).unwrap()[1].as_bytes()).add()?;
+	form.part("questiontext[text]").contents(QUESTION_TEXT.captures(html).unwrap()[1].as_bytes()).add()?;
+	form.part("questiontext[format]").contents(QUESTION_FORMAT.captures(html).unwrap()[1].as_bytes()).add()?;
 	form.part("defaultmark").contents(DEFAULTMARK.captures(html).unwrap()[1].as_bytes()).add()?;
-	form.part("single").contents(SINGLE.captures(html).unwrap()[1].as_bytes()).add()?;
-	form.part("shuffleanswers").contents(SHUFFLEANSWERS.captures(html).unwrap()[1].as_bytes()).add()?;
-	form.part("answernumbering").contents(ANSWERNUMBERING.captures(html).unwrap()[1].as_bytes()).add()?;
+	form.part("single").contents(b"1").add()?;
+	form.part("answernumbering").contents(b"none").add()?;
+	form.part("noanswers").contents(NOANSWERS.captures(html).unwrap()[1].as_bytes()).add()?;
+	form.part("qtype").contents(b"multichoice").add()?;
+	form.part("_qf__qtype_multichoice_edit_form").contents(b"1").add()?;
+	form.part("usecurrentcat").contents(b"1").add()?;
+	form.part("shuffleanswers").contents(b"1").add()?;
+	form.part("penalty").contents(b"0.3333333").add()?;
+
+
 	for (i, cap) in ANSWER_TEXT.captures_iter(html).enumerate() {
-		form.part(&format!("answer[{}][text]", i)).contents(cap[1].as_bytes()).add()?;
-	}
-	for (i, cap) in NOANSWERS.captures_iter(html).enumerate() {
-		form.part(&format!("fraction[{}]", i)).contents(cap[1].as_bytes()).add()?;
-	}
-	form.part("penalty").contents(NOANSWERS.captures(html).unwrap()[1].as_bytes()).add()?;
+	 	form.part(&format!("answer[{}][text]", i)).contents(cap[1].as_bytes()).add()?;
+ }
+	 for (i, cap) in ANSWER_FORMAT.captures_iter(html).enumerate() {
+	 	form.part(&format!("answer[{}][format]", i)).contents(cap[1].as_bytes()).add()?;
+	 }
+	 for (i, cap) in FRACTION.captures_iter(html).enumerate() {
+	 	form.part(&format!("fraction[{}]", i)).contents(cap[1].as_bytes()).add()?;
+	 }
+	 
 	Ok(form)
 }
 
@@ -88,23 +112,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2732%2C21753",
 		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2774%2C21753",
 		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2775%2C21753",
-		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2776%2C21753"
+		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2776%2C21753",
+		"https://lms2.bsuir.by/question/edit.php?courseid=374&category=2801%2C21753",
 		];
-	const COOKIE: &str = "MoodleSession=8r1iheds5bdiu3ef8ni960ifnh";
-	let questions = auth_cookie(TEST_URL[0], COOKIE)?;
-	println!("ПДД: {:?}", questions.find("Дарья"));
-	let gear_regex = Regex::new(r#"<a title="Редактировать".*?href="(.*?)".*?>"#).unwrap();
-	let questions = gear_regex.captures_iter(&questions).map(|s| s[1].to_owned());
-	let mut easy = Easy::new();
-	for q in questions.take(1) {
-		let url = parse_gear_url(&q);
-		println!("{}\n\n\n", url);
-		let html = auth_cookie(&url, COOKIE)?;
-		let form = get_question_form(&html)?;
-		easy.url(&url)?;
-		easy.httppost(form)?;
-		let response = auth_cookie(&url, COOKIE)?;
-		println!("{}", &response);
+	const COOKIE: &str = "MoodleSession=9qevnnjbgqtle18ladl44oq2lt";
+	for test in TEST_URL {
+		println!("Test: {}", test);
+		let questions = get_with_cookie(test, COOKIE)?;
+		println!("ПДД: {:?}", questions.find("Дарья"));
+		let gear_regex = Regex::new(r#"<a title="Редактировать".*?href="(.*?)".*?>"#).unwrap();
+		let questions = gear_regex.captures_iter(&questions).map(|s| s[1].to_owned());
+		for q in questions {
+			let url = parse_gear_url(&q);
+			println!("{}", url);
+			let html = get_with_cookie(&url, COOKIE)?;
+			let form = get_question_form(&html)?;
+			let response = post_with_cookie("https://lms2.bsuir.by/question/question.php", COOKIE, form)?;
+			println!("{}", response.find("Редактировать вопросы").is_some());
+		}
 	}
 	Ok(())
 }
